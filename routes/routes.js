bbf686524d9f181e14f872a2da63c921f4977fea
Kate Burson <burson.kate@gmail.com>
2019-04-24 10:57:29 -0700

    switched scrape site to dezeen

--- routes/routes.js
+++ routes/routes.js
@@ -16,37 +16,36 @@ router.get('/', function(req, res) {
 // A GET route for scraping the echoJS website
 router.get('/scrape', function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get('https://medium.com/topic/design').then(function(response) {
+  axios.get('https://www.dezeen.com/design/').then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
-
+    // Save an empty result object
+    var result = {};
     // Now, we grab every h2 within an article tag, and do the following:
-    $('h3').each(function(i, element) {
-      // Save an empty result object
-      var result = {};
-
+    $('article').each(function(i, element) {
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children('a')
+        .children('header').children('h3')
         .text();
-      result.description = $('.dw.d')
+      result.description = $(this)
         .children('p')
         .text();
       result.link = $(this)
-        .children('a')
+        .children('header').children('a')
         .attr('href');
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, log it
-          console.log(err);
-        });
+      .then(function(dbArticle) {
+        // View the added result in the console
+        console.log(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, log it
+        console.log(err);
+      });
     });
+    
 
     // Send a message to the client
     res.send('Scrape Complete');
